8223a5f74b2808a1600a95c88479fc31ffc825d3
Dmitriy <dmitriyrodionov2015@mail.ru>
2021-10-22 01:14:13 +0300

    сохранение в localstorage

--- js/script.js
+++ js/script.js
@@ -1,10 +1,39 @@
 const input = document.querySelector('.todo__input'),
       taskList = document.querySelector('.todo__task-list'),
-      addBtn = document.querySelector('.todo__add-btn');
+      addBtn = document.querySelector('.todo__add-btn'),
+      saveBtn = document.querySelector('.todo__save'),
+      clearBtn = document.querySelector('.todo__clear');
 
 
-addBtn.addEventListener('click', addTask);      
+addBtn.addEventListener('click', addTask);
+saveBtn.addEventListener('click', saveValues);
+clearBtn.addEventListener('click', clearAll);    
 
+if(localStorage.length != 0) {
+    for(let i = 0; i < localStorage.length; i++) {
+        let k = JSON.parse(localStorage.getItem(i));
+        taskList.insertAdjacentHTML('beforeend', `
+        <div class="todo__task">
+            <input type="checkbox" class="checkbox">
+            <label>${k.value}</label>
+            <img src="img/icons8.svg" alt="clear" class="clear">
+        </div>
+    `);
+        if(k.done == true) {
+            let checkboxes = document.querySelectorAll('.checkbox');
+            checkboxes.forEach((item, r) => {
+                if(r === i) {
+                    item.checked = true;
+                }
+            });
+        }
+    }
+}
+
+const closeBtns = document.querySelectorAll('.clear');
+    if(closeBtns.length != 0) {
+        close();
+    }
 
 function close() {
     const tasks = document.querySelectorAll('.todo__task'),
@@ -20,7 +49,7 @@ function addTask() {
     if(input.value != '') {
         taskList.insertAdjacentHTML('beforeend', `
         <div class="todo__task">
-            <input type="checkbox">
+            <input type="checkbox" class="checkbox">
             <label>${input.value}</label>
             <img src="img/icons8.svg" alt="clear" class="clear">
         </div>
@@ -30,4 +59,27 @@ function addTask() {
     }else {
         input.value = '';
     }
+}
+
+function saveValues() {
+    localStorage.clear();
+    const tasks = document.querySelectorAll('.todo__task');
+    tasks.forEach((item, i) => {
+        let task = {};
+        if(item.firstElementChild.checked) {
+            task.done = true;
+        } else {
+            task.done = false;
+        }
+        task.value = item.firstElementChild.nextElementSibling.textContent;
+        localStorage.setItem(i, JSON.stringify(task));
+    });
+}
+
+function clearAll() {
+    const tasks = document.querySelectorAll('.todo__task');
+    tasks.forEach((item) => {
+        item.remove();
+    });
+    localStorage.clear();
 }
